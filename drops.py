4b15ca131cde5f68726d45865a120a6a6e021e73
audreywl <audrey.lewis@students.olin.edu>
2017-06-05 16:28:14 -0400

    it's raining balls!

---
+++ drops.py
@@ -0,0 +1,64 @@
+import pymunk
+import pygame
+import time
+
+class ShadowSpace(object):
+    """Wrapper for the shadow simulation in pymunk and pygame"""
+    def __init__(self):
+        self.width = 960
+        self.height = 640
+        pygame.init()
+        pygame.display.init()
+        self.surface = pygame.display.set_mode((self.width,self.height))
+        #self.surface = pygame.Surface((720, 640))
+        self.surface.fill((255, 255, 255))
+        self.space = pymunk.Space()
+        self.space.gravity = 0, -800
+        #self.testball = Ball(10, 50)
+        #self.testball.add(self.space)
+        self.balls = self.init_balls(30)
+
+    def init_balls(self, n):
+        ball_list = []
+        for i in range(n):
+            ball = Ball(10, i*self.width/float(n))
+            ball.add(self.space)
+            ball_list.append(ball)
+        return ball_list
+
+    def update(self):
+        self.surface.fill((255, 255, 255))
+        self.space.step(.02)
+        for ball in self.balls:
+            ball.draw(self.surface)
+        pygame.display.update()
+
+
+class Ball(object):
+    """it's a ball. it's got image and physics attributes"""
+    def __init__(self, radius=10, x=0, y=0, color=(0,0,0), mass=1):
+        self.radius = radius
+        self.x = x
+        self.y = y
+        self.color = color
+        self.mass = mass
+        self.moment = pymunk.moment_for_circle(mass, 0, radius)
+        self.body = pymunk.Body(self.mass, self.moment)
+        self.shape = pymunk.Circle(self.body, self.radius)
+        self.body.position = self.x, self.y
+
+    def add(self, space):
+        space.add(self.body, self.shape)
+    def draw(self, surface):
+        self.x, self.y = self.body.position
+        #print self.y
+        pygame.draw.circle(surface, self.color, (int(self.x), abs(int(self.y))), self.radius)
+
+if __name__ == '__main__':
+    testSpace = ShadowSpace()
+    while True:
+        #testSpace.drawCircle()
+
+        #pygame.display.update()
+        testSpace.update()
+        time.sleep(.01)
