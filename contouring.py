bc5f67fb68f839ed11e9544c866de6da56e9efc7
Elepert <emily.lepert@students.olin.edu>
2017-06-06 11:14:42 -0400

    started adding pymunk

--- contouring.py
+++ contouring.py
@@ -1,41 +1,64 @@
 
 import numpy as np
 import cv2
+import cv2.cv as cv
+import pymunk
 
 
-cap = cv2.VideoCapture(0)
+class Contour(object):
 
-while(True):
-	# Capture frame-by-frame
-    ret, frame = cap.read()
+    def __init__(self, mass):
+        self.something = 0
+        self.body = pymunk.Body(mass)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
+    def video_contour(self):
+        cap = cv2.VideoCapture(0)
 
-    lower_color = np.array([0,0,0], dtype = "uint8")
-    upper_color = np.array([110,110,110], dtype = "uint8")
+        while(True):
+        	# Capture frame-by-frame
+            ret, frame = cap.read()
 
+            # transform image to grayscale and blur
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            blur = cv2.GaussianBlur(gray,(5,5),0)
 
-    #ret, thresh_img = cv2.threshold(blur,177,255,cv2.THRESH_BINARY)
+            # color thresholds for binary image
+            lower_color = np.array([0,0,0], dtype = "uint8")
+            upper_color = np.array([115,115,115], dtype = "uint8")
 
-    #th3 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            #cv2.THRESH_BINARY,11,2)
+            # create binary image
+            binary_img = cv2.inRange(blur, lower_color, upper_color)
 
-    binary_img = cv2.inRange(blur, lower_color, upper_color)
+            # dilate image
+            kernel = np.ones((5,5), np.uint8)
+            img_erode = cv2.erode(binary_img, kernel, iterations=1)
+            img_dilation = cv2.dilate(img_erode, kernel, iterations=1)
+            #opening = cv2.morphologyEx(img_dilation, cv2.MORPH_OPEN, kernel)
 
-    kernel = np.ones((5,5), np.uint8)
-    img_dilation = cv2.dilate(binary_img, kernel, iterations=1)
-    #contours =  cv2.findContours(binary_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]
-   # for c in contours:
-        #cv2.drawContours(binary_img, [c], -1, (0,255,0), 3)
+            # find thresholds for contouring
+            ret,thresh = cv2.threshold(opening,0,115,cv2.THRESH_BINARY)
 
-     # Display the resulting frame
-    cv2.imshow('frame',img_dilation)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-#erod and dilateb
+            # contour
+            contours, hierarchy= cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            #self.contour_object(contours)
+            # draw contour on the dilated image
+            for c in contours:
+                cv2.drawContours(frame, [c], -1, (255,0,0), 3)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+             # Display the resulting frame
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        #erod and dilateb
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def contour_object(self, contour_list):
+        self.contour = pymunk.Poly(Body, contour_list)
+        print(self.contour)
+
+if __name__ == '__main__':
+    testContour = Contour(1)
+    testContour.video_contour()
