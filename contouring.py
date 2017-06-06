ca6bf0562999d0a9171c81bf6482a5a4c2429a6f
audreywl <audrey.lewis@students.olin.edu>
2017-06-06 14:13:45 -0400

    removed run loop for this branch

--- contouring.py
+++ contouring.py
@@ -8,63 +8,50 @@ class Contour(object):
     def __init__(self, mass):
         self.something = 0
         self.body = pymunk.Body(mass)
+        self.cap = cv2.VideoCapture(0)
+        self.shadow_detected = False
 
-    def video_contour(self):
-        cap = cv2.VideoCapture(0)
+    def create_shadow(self, contour):
+        pass
+    def update_contours(self):
+    	# Capture frame-by-frame
+        ret, frame = cap.read()
 
-        while(True):
-        	# Capture frame-by-frame
-            ret, frame = cap.read()
+        # transform image to grayscale and blur
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        blur = cv2.GaussianBlur(gray,(5,5),0)
 
-            # transform image to grayscale and blur
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            blur = cv2.GaussianBlur(gray,(5,5),0)
+        # color thresholds for binary image
+        lower_color = np.array([0,0,0], dtype = "uint8")
+        upper_color = np.array([90,90,90], dtype = "uint8")
 
-            # color thresholds for binary image
-            lower_color = np.array([0,0,0], dtype = "uint8")
-            upper_color = np.array([90,90,90], dtype = "uint8")
+        # create binary image
+        binary_img = cv2.inRange(blur, lower_color, upper_color)
 
-            # create binary image
-            binary_img = cv2.inRange(blur, lower_color, upper_color)
+        # dilate image
+        # kernel = np.ones((5,5), np.uint8)
+        # img_erode = cv2.erode(binary_img, kernel, iterations=0)
+        # img_dilation = cv2.dilate(img_erode, kernel, iterations=0)
 
-            # dilate image
-            kernel = np.ones((5,5), np.uint8)
-            img_erode = cv2.erode(binary_img, kernel, iterations=0)
-            img_dilation = cv2.dilate(img_erode, kernel, iterations=0)
-            #opening = cv2.morphologyEx(img_dilation, cv2.MORPH_OPEN, kernel)
-            cv2.imshow('binary',img_dilation)
+        # contour
+        if cv2.__version__.startswith('3.'):
+             _, contours, hierarchy= cv2.findContours(img_dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            contours, hierarchy= cv2.findContours(img_dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-            # find thresholds for contouring
-            #ret,thresh = cv2.threshold(img_dilation,0,115,cv2.THRESH_BINARY)
+        tuple_contours = self.convert_contour(contours)
+        if not self.shadow_detected:
+            self.contour_shape = pymunk.Poly(self.body, tuple_contours)
 
-            # contour
-            if cv2.__version__.startswith('3.'):
-                 _, contours, hierarchy= cv2.findContours(img_dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-            else:
-                contours, hierarchy= cv2.findContours(img_dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-            #self.contour_object(contours)
-            tuple_contours = self.convert_contour(contours)
-            self.update_contour_object(tuple_contours)
-            # draw contour on the dilated image
-            for c in contours:
-                cv2.drawContours(frame, [c], -1, (255,0,0), 3)
-
-             # Display the resulting frame
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
+    def kill_video(self):
         # When everything done, release the capture
         cap.release()
         cv2.destroyAllWindows()
 
-    def update_contour_object(self, contour_list):
-        self.contour = pymunk.Poly(self.body, contour_list)
-        #print(self.contour)
 
     def convert_contour(self, contour_list):
         contour_lst_of_tuples = []
+        print contour_list
         for i in contour_list:
             for j in i:
                 for k in j:
@@ -73,4 +60,4 @@ class Contour(object):
 
 if __name__ == '__main__':
     testContour = Contour(1)
-    testContour.video_contour()
+    testContour.update_contours()
